9c12e89aa6996301c82ced756cf499266facd4a7
Supakarn Laorattanakul <wearedprompt@gmail.com>
2021-11-16 23:31:15 +0700

    Almost Final

--- MPI/mpi_test.c
+++ MPI/mpi_test.c
@@ -22,7 +22,8 @@ char *destFileName(char *fileName) {
 
 // Operates XOR
 void xorOperation(char *destBuffer, char *srcBuffer, int size, int key) {
-  for (int i = 0; i < size; i++) {
+  int i;
+  for (i = 0; i < size; i++) {
     destBuffer[i] = srcBuffer[i] ^ key;
     // printf("%d : %d %d\n", i, srcBuffer[i], destBuffer[i]);
   }
@@ -33,6 +34,7 @@ int main(int argc, char **argv) {
   int numOfProcesses;
   double start, end;
   int size;
+  int i;
 
   int key = atoi(argv[1]);
   char *fileName = argv[2];
@@ -40,21 +42,15 @@ int main(int argc, char **argv) {
   char *oFileName = destFileName(fileName);
   FILE *inputFile = fopen(fileName, "rb");
 
-  char buffer[size];
-  char operatedBuffer[size];
-
-  // inputFile =
   if (!inputFile) {
     fprintf(stderr, "Unable to open file\n");
     return -1;
   }
 
   size = fileSize(inputFile);
-  for (int i = 0; i < size; i++) {
-    buffer[i] = 1;
-    // printf("%d : %d\n", i, buffer[i]);
-  }
-  printf("Size: %d\n", size);
+
+  char buffer[size];
+  char operatedBuffer[size];
 
   fread(&buffer, size, 1, inputFile);
   fclose(inputFile);
@@ -65,33 +61,30 @@ int main(int argc, char **argv) {
   MPI_Comm_rank(MPI_COMM_WORLD, &processId);       // Get Current Process ID
   MPI_Comm_size(MPI_COMM_WORLD, &numOfProcesses);  // Get a size of processes requested by the command -np
 
-  // printf("%d\n", processId);
-  // printf("%d\n", numOfProcesses);
-
-  if (processId == 0) {
-    for (int i = 0; i < size; i++) {
-      printf("%d : %d\n", i, buffer[i]);
-    }
-    printf("%d\n", size);
-  }
-
   start = MPI_Wtime();
 
   int localMaxArraySize = size % numOfProcesses == 0 ? size / numOfProcesses : (size / numOfProcesses) + 1;
-  // char localBuffer[localMaxArraySize];
   char localBuffer[localMaxArraySize];
 
   MPI_Scatter(buffer, localMaxArraySize, MPI_CHAR, &localBuffer, localMaxArraySize, MPI_CHAR, 0, MPI_COMM_WORLD);
-  // xorOperation(operatedBuffer, buffer, localMaxArraySize, key);
-  for (int i = 0; i < localMaxArraySize; i++) {
-    localBuffer[i] = localBuffer[i];
-    printf("%d %d : %d\n", processId, i, localBuffer[i]);
-  }
+  printf("[Process %d] Starting Encryption/Decryption with Xor Operation\n", processId);
+  xorOperation(operatedBuffer, buffer, localMaxArraySize, key);
+  printf("[Process %d] Finishing Encryption/Decryption with Xor Operation\n", processId);
   MPI_Gather(&localBuffer, localMaxArraySize, MPI_CHAR, operatedBuffer, localMaxArraySize, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+  if (processId == 0) {
+    FILE *outputFile = fopen(oFileName, "wb");
+    if (!outputFile) {
+      fprintf(stderr, "Unable to create file\n");
+      return -1;
+    }
+    fwrite(&operatedBuffer, 1, size, outputFile);
+    fclose(outputFile);
+  }
 
   end = MPI_Wtime();
 
-  // printf("[Process %d] Execution time: %f seconds with %d processes and %d random integers.\n", processId, end - start, numOfProcesses, localMaxArraySize);
+  printf("[Process %d] Execution time: %f seconds with %d processes.\n", processId, end - start, numOfProcesses);
 
   MPI_Finalize();  // Finalize MPI Program
   return 0;
